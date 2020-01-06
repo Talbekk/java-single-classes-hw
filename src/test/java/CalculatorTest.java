de44d7201d969841a7d6576ecb6c0f61c26e0bce
Talbekk <mt.campbell@outlook.com>
2020-01-06 15:15:53 +0000

    passes all tests

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -13,19 +13,19 @@ public class CalculatorTest {
     public void addSum(){
         assertEquals(4.00, calculator.add(2.00, 2.00), 0.01);
     }
-//
-//    @Test
-//    public void subtractSum(){
-//        assertEquals(1.00, calculator.subtract(5.00, 4.00), 0.01);
-//    }
-//
-//    @Test
-//    public void multiplySum(){
-//        assertEquals(100.00, calculator.multiply(5.00, 20.00), 0.01);
-//    }
-//
-//    @Test
-//    public void divideSum(){
-//        assertEquals(17.69, calculator.divide(568.86, 32.14), 0.01);
-//    }
+
+    @Test
+    public void subtractSum(){
+        assertEquals(1.00, calculator.subtract(5.00, 4.00), 0.01);
+    }
+
+    @Test
+    public void multiplySum(){
+        assertEquals(100.00, calculator.multiply(5.00, 20.00), 0.01);
+    }
+
+    @Test
+    public void divideSum(){
+        assertEquals(17.69, calculator.divide(568.86, 32.14), 0.01);
+    }
 }
