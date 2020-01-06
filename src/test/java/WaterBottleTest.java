9e63d2bb4e5a1c14f5449db2a415413a60288409
Talbekk <mt.campbell@outlook.com>
2020-01-06 15:22:13 +0000

    Create Bottle test

---
+++ src/test/java/WaterBottleTest.java
@@ -0,0 +1,12 @@
+import org.junit.Before;
+
+public class WaterBottleTest {
+
+    WaterBottle  waterBottle;
+
+    @Before
+    public void before(){
+        waterBottle = new WaterBottle(100.00);
+    }
+
+}
