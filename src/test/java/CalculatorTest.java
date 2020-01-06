43c7815c19933b54231f8d1ff74c13e260e59e98
Talbekk <mt.campbell@outlook.com>
2020-01-06 15:06:07 +0000

    setup divide test

---
+++ src/test/java/CalculatorTest.java
@@ -0,0 +1,31 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class CalculatorTest {
+    Calculator calculator;
+    @Before
+    public void before(){
+        calculator = new Calculator();
+    }
+    @Test
+    public void addSum(){
+        assertEquals(4.00, calculator.add(2.00, 2.00), 0.01);
+    }
+
+    @Test
+    public void subtractSum(){
+        assertEquals(1.00, calculator.subtract(5.00, 4.00), 0.01);
+    }
+
+    @Test
+    public void multiplySum(){
+        assertEquals(100.00, calculator.multiply(5.00, 20.00), 0.01);
+    }
+
+    @Test
+    public void divideSum(){
+        assertEquals(17.69, calculator.divide(568.86, 32.14), 0.01);
+    }
+}
